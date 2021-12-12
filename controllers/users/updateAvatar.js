b471ac85104bc79bdc356063ddc2d89231c62796
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-12-12 17:21:43 +0200

    update

---
+++ controllers/users/updateAvatar.js
@@ -0,0 +1,25 @@
+const { User } = require('../../models');
+const path = require('path');
+const fs = require('fs/promises');
+
+const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
+
+const updateAvatar = async (req, res) => {
+    const { path: tempUpload, originalname } = req.file;
+    const { _id: id } = req.user;
+
+    try {
+        const newAvatarName = `${id}_${originalname}`;
+        const resultUpload = path.join(avatarsDir, newAvatarName);        
+        await fs.rename(tempUpload, resultUpload);
+        const avatarURL = path.join('public', 'avatars', newAvatarName);
+        await User.findByIdAndUpdate(req.user._id, { avatarURL });
+        res.json({ avatarURL });
+
+    } catch (err) {
+        await fs.unlink(tempUpload);
+        throw err;
+    }
+};
+
+module.exports = updateAvatar;
