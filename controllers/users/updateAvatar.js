ea2f90b655d996aa986184dc762183c9f7e6d087
Nazar Khizhniak <77328105+Nabiros@users.noreply.github.com>
2021-12-17 17:45:04 +0200

    fix updateAvatar

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -7,9 +7,10 @@ const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 const updateAvatar = async (req, res) => {
     const { path: tempUpload, originalname } = req.file;
     const { _id: id } = req.user;
-
+    const newAvatarName = `${id}_${originalname}`;
+    
     try {
-        const newAvatarName = `${id}_${originalname}`;
+        
         const resultUpload = path.join(avatarsDir, newAvatarName);        
         await fs.rename(tempUpload, resultUpload);
         const avatarURL = path.join('public', 'avatars', newAvatarName);
